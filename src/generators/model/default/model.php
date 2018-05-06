2a88052d24329cd06fe6998928ceb06e12276214
Janagaran <vjanagaran@gmail.com>
2018-05-07 02:08:43 +0530

    behaviors() added

--- src/generators/model/default/model.php
+++ src/generators/model/default/model.php
@@ -16,17 +16,25 @@
 
 echo "<?php\n";
 ?>
-
+/*
+ * Project : <?= Yii::$app->name ?><?= "\n" ?>
+ * Class : <?= $generator->ns ?>\<?= $className ?><?= "\n" ?>
+ * Author : Janagaran V (vjanagaran@gmail.com)
+ * Created : <?= date("d-M-Y H:i") ?><?= "\n" ?>
+ */
+ 
 namespace <?= $generator->ns ?>;
 
 use Yii;
+<?php if (isset($properties['created_at']) || isset($properties['updated_at'])): ?>
+use yii\behaviors\TimestampBehavior;
+<?php endif; ?>
+<?php if (isset($properties['created_by']) || isset($properties['updated_by'])): ?>
+use yii\behaviors\BlameableBehavior;
+<?php endif; ?>
 
 /**
- * Project : <?= ${project} ?>
- * Class : <?= $generator->ns ?>/<?= $className ?>
- * Author : Janagaran V (vjanagaran@gmail.com)
- * Created : <?= date("d-M-Y H:i P") ?>
- * 
+ *
  * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
  *
 <?php foreach ($properties as $property => $data): ?>
@@ -39,7 +47,7 @@ use Yii;
 <?php endforeach; ?>
 <?php endif; ?>
  */
-class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . "\n" ?> {
+class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') ?> {
     /**
      * {@inheritdoc}
      */
@@ -82,6 +90,53 @@ class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . 
         <?= $relation[0] . "\n" ?>
     }
 <?php endforeach; ?>
+
+<?php if (isset($properties['created_by']) || isset($properties['created_at'])
+        || isset($properties['updated_by']) || isset($properties['updated_at'])):  ?>
+
+    /**
+     * @inheritdoc
+     * @return array mixed
+     */
+    public function behaviors()
+    {
+        return [
+<?php if (isset($properties['created_at']) || isset($properties['updated_at'])):?>
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+<?php if (isset($properties['created_at'])):?>
+                'createdAtAttribute' => 'created_at',
+<?php else :?>
+                'createdAtAttribute' => false,
+<?php endif; ?>
+<?php if (isset($properties['updated_at'])):?>
+                'updatedAtAttribute' => 'updated_at',
+<?php else :?>
+                'updatedAtAttribute' => false,
+<?php endif; ?>
+                'value' => new Expression('NOW()'),
+            ],
+<?php endif; ?>
+<?php if (isset($properties['created_by']) || isset($properties['updated_by'])):?>
+            'blameable' => [
+                'class' => BlameableBehavior::className(),
+<?php if (isset($properties['created_by'])):?>
+                'createdByAttribute' => 'created_by',
+<?php else :?>
+                'createdByAttribute' => false,
+<?php endif; ?>
+<?php if (isset($properties['updated_by'])):?>
+                'updatedByAttribute' => 'updated_by',
+<?php else :?>
+                'updatedByAttribute' => false,
+<?php endif; ?>
+                'value' => '\\Yii::$app->admin->id',
+            ],
+<?php endif; ?>
+        ];
+    }
+<?php endif; ?>
+    
 <?php if ($queryClassName): ?>
 <?php
     $queryClassFullName = ($generator->ns === $generator->queryNs) ? $queryClassName : '\\' . $generator->queryNs . '\\' . $queryClassName;
